b857d41f4524d6ecadf5fd6b114d155faf5f4e8e
bby <603086442@qq.com>
2020-04-25 18:13:13 +0800

    指针一

--- C语言进阶.cpp
+++ C语言进阶.cpp
@@ -140,44 +140,148 @@
 //}
 
 //判断m是否为素数
-bool IsPrime(int m)
+//bool IsPrime(int m)
+//{
+//	int i;
+//
+//	for (i = 2; i < m; i++)
+//	{
+//		if (m % i == 0)
+//			break;
+//	}
+//	if (i == m)
+//		return true;
+//	else
+//		return false;
+//}
+//
+////把1到n之间所有的素数在显示器上输出
+//void TraverseVal(int n)
+//{
+//	int i;
+//
+//	for (i = 2; i <= n; i++)
+//	{
+//		if (IsPrime(i))
+//			printf("%d\n", i);
+//	}
+//}
+//
+//int main(void)
+//{
+//	int val;
+//
+//	scanf_s("%d", &val);
+//
+//	TraverseVal(val);
+//
+//	return 0;
+//}
+
+////指针热身
+//int main(void)
+//{
+//	int* p;  //p是变量的名字，int* 表示p变量存放的是int类型变量的地址
+//	//int* p  :  p是变量名，p变量的数据类型是int*类型，所谓int*类型就是存放int变量地址的类型
+//	int i = 3;
+//	int j;
+//	//p = &i;  //正确
+//	////p = i;  //错误，p只能存放int类型变量的地址，不能存放int类型变量的值
+//	////p = 55; //错误，原因同上
+//
+//	p = &i;
+//	/*
+//		1.p保存了i的地址，因此p指向i;
+//		2.p不是i，i也不是p，更准确地说，修改p的值，不影响i的值，修改i的值也不会影响p的值
+//		3.如果一个指针变量指向了某个普通变量，则*指针变量就完全等同于普通变量
+//			例：
+//				如果p是个指针变量，并且存放了普通变量i的地址，则p指向了普通变量i
+//				*p 就完全等同于 i
+//				或者说：在所有出现*p的地方都可以替换成i
+//						在所有出现i的地方都可以替换成*p
+//
+//			 *p 就是以p的内容为地址的变量
+//	*/
+//
+//	j = *p; //等价于j = i
+//	printf("i = %d, j = %d\n", i, j);
+//
+//
+//
+//	return 0;
+//}
+
+//指针常见错误
+//int main(void)
+//{
+//	int* p;
+//	int i = 5;
+//
+//	* p = i; //错误
+//	printf("%d\n", *p);
+//
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int i = 5;
+//	int* p;
+//	int* q;
+//
+//	p = &i;
+//	//*q = p; //错误 类型不一样
+//	//*q = *p;  //错误
+//	p = q;  //错误 q是垃圾值，q赋给p，p也变成垃圾值
+//
+//	printf("%d\n", *q); //q的空间是属于本程序的，所以本程序可以读写q的内容，但如果q内部是垃圾值，则本程序不能读写*p的内容。因为*q所代表的内存单元的控制权限别没有分配给本程序，所以本程序运行到237行时就会立即出错
+//
+//	return 0;
+//}
+
+//经典指针程序_互换两个数字
+
+//不能完成互换功能
+//void exchange(int a, int b)
+//{
+//	int t;
+//
+//	t = a;
+//	a = b;
+//	b = t;
+//
+//	return;
+//}
+
+//不能完成互换
+//void exchange(int* p, int* q)
+//{
+//	int* t;
+//
+//	t = p;
+//	p = q;
+//	q = t;
+//}
+
+//可以完成互换功能
+void exchange(int* p, int* q)
 {
-	int i;
+	int t;
 
-	for (i = 2; i < m; i++)
-	{
-		if (m % i == 0)
-			break;
-	}
-	if (i == m)
-		return true;
-	else
-		return false;
-}
-
-//把1到n之间所有的素数在显示器上输出
-void TraverseVal(int n)
-{
-	int i;
-
-	for (i = 2; i <= n; i++)
-	{
-		if (IsPrime(i))
-			printf("%d\n", i);
-	}
+	t = *p;    // p是int*， *p是int
+	*p = *q;
+	*q = t;
 }
 
 int main(void)
 {
-	int val;
+	int a = 3;
+	int b = 5;
+	
+	exchange(&a, &b);
 
-	scanf_s("%d", &val);
+	printf("a=%d b=%d\n", a, b);
 
-	TraverseVal(val);
-
-	return 0;
 }
-
-
 
 
